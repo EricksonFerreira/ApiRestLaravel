fc64a19b543bf05162ebf3a1e343cc47431c06fc
Erickson <erickson.rinho@hotmail.com>
2019-06-28 16:18:20 -0300

    Adicionando o arquivo Python

---
+++ python.py
@@ -0,0 +1,7 @@
+import requests
+# r = requests.get('http://localhost:8000/api/jogos/')
+# r = requests.get('http://localhost:8000/api/jogos/show/1')
+# r = requests.post('http://localhost:8000/api/jogos/store/?nome=futebol')
+# r = requests.put('http://localhost:8000/api/jogos/update/1?nome=basquete')
+# r = requests.delete('http://localhost:8000/api/jogos/destroy/1')
+print (r.text)
